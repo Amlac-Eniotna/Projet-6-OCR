def11661943f8694e95df12029d2640a56cfeec1
Amlac Eniotna <antoine.calma30@gmail.com>
2023-12-13 16:36:02 +0100

    ajout clic sur les btn filtres et filtrage travaux

--- FrontEnd/js/filters.js
+++ FrontEnd/js/filters.js
@@ -1,10 +1,14 @@
+import { filteredWork } from "./works.js";
+
+// gestion des filtres
 export function filters(worksList) {
     let listFilter = whatFilters(worksList);
-    createButtonFilters(listFilter);
+    createBtnFilters(listFilter);
+    clickBtnFilters();
 }
 
 /**
- * récupère la liste des travaux et renvoi la catégorie des travaux connaitre les filtres à appliquer
+ * détermine une liste de filtres
  * @param {object} worksList - liste des travaux
  * @returns {array} liste de noms des filtres
  */
@@ -24,24 +28,46 @@ function whatFilters(worksList) {
     return listFilter;
 }
 
-function createButtonFilters(listFilter) {
-    let list = document.createElement("ul");
-    list.id = "listFilters";
+/**
+ * création des boutons filtres
+ * @param {array} listFilter - liste des noms des filtres
+ */
+function createBtnFilters(listFilter) {
+    let listBtnFilter = document.createElement("ul");
+    listBtnFilter.className = "list-filters";
     let portfolio = document.getElementById("portfolio");
-    portfolio.insertBefore(list, portfolio.children[1]);
+    portfolio.insertBefore(listBtnFilter, portfolio.children[1]);
 
     let btnFilter = document.createElement("li");
     btnFilter.innerText = "Tous";
-    btnFilter.className = "filters selectedFilters";
-    list.appendChild(btnFilter);
+    btnFilter.className = "list-filters__filters list-filters__selectedFilters";
+    listBtnFilter.appendChild(btnFilter);
 
     for(let i = 0; i < listFilter.length; i++) {
         btnFilter = document.createElement("li");
         btnFilter.innerText = listFilter[i];
-        btnFilter.className = "filters";
+        btnFilter.className = "list-filters__filters";
         if(listFilter[i].length > 7){
-            btnFilter.classList.add("longFilters")
+            btnFilter.classList.add("list-filters__longFilters");
         }
-        list.appendChild(btnFilter);
+        listBtnFilter.appendChild(btnFilter);
+    }
+}
+
+/**
+ * fonction qui a l'aide d'un event listener change le filtres selectionné en utilisant la classe
+ */
+function clickBtnFilters() {
+    let btnFilter = document.querySelectorAll(".list-filters__filters");
+    for(let i = 0; i < btnFilter.length; i++) {
+        btnFilter[i].addEventListener("click", (event) => {
+            if (!(event.target.classList.contains('list-filters__selectedFilters'))) {
+                for (let j = 0; j < btnFilter.length; j++) {
+                    btnFilter[j].classList.remove("list-filters__selectedFilters");
+                }
+                event.target.classList.add("list-filters__selectedFilters");
+            }
+            filteredWork();
+        })
     }
 }
--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -1,10 +1,14 @@
 import { filters } from "./filters.js"
 
+//pour faire des transfert
+let worksListGlobale;
+
 export async function works() {
     resetFrontWorks();
     let worksList = await getWorks();
-    addWorks(worksList);
+    worksListGlobale = worksList;
     filters(worksList);
+    changeWorkByFilter("Tous");
 }
 
 /**
@@ -12,8 +16,8 @@ export async function works() {
  * @returns {object} liste des travaux
  */
 async function getWorks() {
-    let worksList = await fetch("http://localhost:5678/api/works").then(worksList => worksList.json()).catch(error => console.error(error));
-    return(worksList);
+    let worksListFromApi = await fetch("http://localhost:5678/api/works").then(worksList => worksList.json()).catch(error => console.error(error));
+    return (worksListFromApi);
 }
 
 // effacer le front deja présent
@@ -24,20 +28,55 @@ function resetFrontWorks() {
 
 /**
  * fonction d'affichages des photo
- * @param {object} worksList liste des travaux
+ * @param {object} worksList - liste des travaux
+ * @param {number} i - index du travail a partir de 0
  */
-function addWorks(worksList) {
+function addWorks(worksList, i) {
+    let pictureWork = document.createElement("img");
+    pictureWork.src = worksList[i].imageUrl;
+    pictureWork.alt = worksList[i].title;
+    pictureWork.crossOrigin = "anonymous";
+    let titleWork = document.createElement("figcaption");
+    titleWork.innerText = worksList[i].title;
+    let work = document.createElement("figure");
+    work.appendChild(pictureWork);
+    work.appendChild(titleWork);
+    let gallery = document.querySelector("#portfolio .gallery");
+    gallery.appendChild(work);
+}
+
+/* --------------------------------------------------------------------------------------- */
+
+
+export function filteredWork() {
+    let filterSelected = whatFilterSelected();
+    changeWorkByFilter(filterSelected);
+}
+
+/**
+ * récupère le nom du filtres
+ * @returns nom du filtre
+ */
+function whatFilterSelected() {
+    let btnFilter = document.querySelectorAll(".list-filters__filters");
+    for (let i = 0; i < btnFilter.length; i++) {
+        if (btnFilter[i].classList.contains('list-filters__selectedFilters'))
+            return(btnFilter[i].innerText);
+    }
+}
+
+/**
+ * affiche les travaux en fontion du filtre selectionné
+ * @param {string} filterSelected - nom du filtre selectionné 
+ */
+function changeWorkByFilter(filterSelected) {
+    let worksList = worksListGlobale;
+    resetFrontWorks();
     for (let i = 0; i < worksList.length; i++) {
-        let pictureWork = document.createElement("img");
-        pictureWork.src = worksList[i].imageUrl;
-        pictureWork.alt = worksList[i].title;
-        pictureWork.crossOrigin = "anonymous";
-        let titleWork = document.createElement("figcaption");
-        titleWork.innerText = worksList[i].title;
-        let work = document.createElement("figure");
-        work.appendChild(pictureWork);
-        work.appendChild(titleWork);
-        let gallery = document.querySelector("#portfolio .gallery");
-        gallery.appendChild(work);
+        if(worksList[i].category.name === filterSelected) {
+            addWorks(worksList, i);
+        } else if (filterSelected === "Tous") {
+            addWorks(worksList, i);
+        }
     }
 }
